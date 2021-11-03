02c529e71cbdb4d41cbf545de00599660ae61ba9
shivam mehta <74263701+shivam946@users.noreply.github.com>
2021-11-03 20:59:09 +0000

    Add files via upload

---
+++ RentACar/src/ooc/yoursolution/RentACar.java
@@ -0,0 +1,115 @@
+/*
+Below  is the github repository link.
+
+https://github.com/shivam946/CarRentalSystem.git
+*/
+package ooc.yoursolution;
+
+
+import java.util.List;
+import ooc.enums.*;
+
+public class RentACar implements RentACarInterface {
+
+    private List<Car> cars;
+    private String name;
+
+    public RentACar(List<Car> cars, String name) {
+        this.cars = cars;
+        this.name = name;
+    }
+
+    @Override
+    public List getCars() {
+        return cars;
+    }
+
+    @Override
+    public void setCars(List cars) {
+        this.cars = cars;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean checkAvailability(Month month, int day, Make make, int lengthOfRent) {
+        int currentDay, flag;
+
+        for (Car car : cars) {
+            if (car.getMake().equals(make)) {
+                flag = 0;
+                currentDay = day;
+                for (int i=0; i<lengthOfRent; i++) {
+                    if (!car.isAvailable(month, currentDay++)) {
+                        flag = 1;
+                        break;
+                    }
+                }
+
+                if (flag == 0) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    @Override
+    public int getCarAvailable(Month month, int day, Make make, int lengthOfRent) {
+        int currentDay, flag;
+
+        for (Car car : cars) {
+            if (car.getMake().equals(make)) {
+                flag = 0;
+                currentDay = day;
+                for (int i=0; i<lengthOfRent; i++) {
+                    if (!car.isAvailable(month, currentDay++)) {
+                        flag = 1;
+                        break;
+                    }
+                }
+
+                if (flag == 0) {
+                    return car.getId();
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    @Override
+    public boolean bookCar(Month month, int day, Make make, int lengthOfRent) {
+
+        if (!checkAvailability(month, day, make, lengthOfRent)) {
+            return false;
+        }
+
+        int carId = getCarAvailable(month, day, make, lengthOfRent);
+
+        for (Car car : cars) {
+            if (car.getId() == carId && car.getMake() == make) {
+                int currentDay = day;
+                for (int i=0; i<lengthOfRent; i++) {
+                    car.book(month, currentDay++);
+                }
+            }
+        }
+
+        return true;
+    }
+
+    @Override
+    public int getNumberOfCars() {
+        return cars.size();
+    }
+}
